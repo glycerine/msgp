8baf9c43c88315b2b11266e3d984d435d7efb662
Jason E. Aten <j.e.aten@gmail.com>
2016-10-16 00:33:49 -0500

    omitempty tags implemented
    struct fields can be tagged with
     `msg:",omitempty"`
    
    Such fields will not be serialized
    if they contain their zero values.
    
    For structs with many optional
    fields, the space and time savings
    can be substantial.
    
    From a zero-alloc standpoint,
    UnmarshalMsg and DecodeMsg continue,
    as before, to re-use existing fields in an
    object rather than allocating new
    ones. So, if you decode into the same
    object repeatedly, things like slices
    maps, and fields that point to other
    structures won't be re-allocated.
    
    Instead, maps and fields will be re-sized
    appropriately. In other words, mutable
    fields are simply mutated in-place.
    
    Fixes #103

--- _generated/gen_test.go
+++ _generated/gen_test.go
@@ -98,8 +98,8 @@ func Test1EncodeDecode(t *testing.T) {
 	}
 
 	if !reflect.DeepEqual(tt, tnew) {
-		t.Logf("in: %v", tt)
-		t.Logf("out: %v", tnew)
+		t.Logf("in: %#v", tt)
+		t.Logf("out: %#v", tnew)
 		t.Fatal("objects not equal")
 	}
 
@@ -118,8 +118,8 @@ func Test1EncodeDecode(t *testing.T) {
 	}
 
 	if !reflect.DeepEqual(tt, tanother) {
-		t.Logf("in: %v", tt)
-		t.Logf("out: %v", tanother)
+		t.Logf("in: %#v", tt)
+		t.Logf("out: %#v", tanother)
 		t.Fatal("objects not equal")
 	}
 }
